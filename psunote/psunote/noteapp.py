1467baa422b877af0b2e6a6f554719cf3670a711
ภูรินทร์ นิกพงศ์ <123phoorin@gmail.com>
2023-10-20 21:06:03 +0700

    Create tag-edit fuction

--- psunote/psunote/noteapp.py
+++ psunote/psunote/noteapp.py
@@ -4,6 +4,7 @@ import models
 import forms
 
 from sqlalchemy.sql import func
+
 app = flask.Flask(__name__)
 app.config["SECRET_KEY"] = "This is secret key"
 app.config[
@@ -91,6 +92,15 @@ def notes_update(note_id):
         form=form,
     )
 
+@app.route("/notes/<int:note_id>/delete", methods=["GET", "POST"])
+def notes_delete(note_id):
+    db = models.db
+    note = db.session.query(models.Note).get(note_id)
+    db.session.delete(note)
+    db.session.commit()
+
+    return flask.redirect(flask.url_for("index"))
+
 @app.route("/tags/<tag_name>")
 def tags_view(tag_name):
     db = models.db
@@ -109,6 +119,40 @@ def tags_view(tag_name):
         notes=notes,
     )
 
+@app.route("/tags/management")
+def tags_management():
+    db = models.db
+    tags = db.session.execute(
+        db.select(models.Tag)
+    ).scalars()
+
+    return flask.render_template(
+        "tags-management.html",
+        tags=tags,
+    )
+
+@app.route("/tags/<tag_name>/update", methods=["GET","POST"])
+def tags_update(tag_name):
+    db = models.db
+    form = forms.TagForm()
+    tag = db.session.execute(
+        db.select(models.Tag).where(models.Tag.name == tag_name)
+    ).scalars().first()
+
+    if tag :
+        if form.validate_on_submit():
+            tag.name = form.name.data
+            db.session.commit()
+            return flask.redirect(flask.url_for("index"))
+        
+        form.name.data = tag.name
+    
+    return flask.render_template(
+        "tags-edit.html",
+        tag=tag,
+        form=form,
+        tag_name=tag_name,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
